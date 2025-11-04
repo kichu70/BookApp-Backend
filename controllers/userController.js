cce504888573dd78d613a1e6742c68a1be8ca388
Ansif <kiichu7034@gmail.com>
2025-11-04 16:04:58 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,4 @@
-// import user from "../models/user.js";
-import user from "../models/user.js"
-
+import user from "../models/user.js";
 import bcrypt from "bcryptjs";
 import { validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
